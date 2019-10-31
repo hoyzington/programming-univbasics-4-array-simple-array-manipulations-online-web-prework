8bd927c51cc65905abb024611bb0f3dddf2c58f2
Scott Hoisington <hoyzington@gmail.com>
2019-10-31 20:02:09 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,59 +1,47 @@
 def using_push(array, string)
-  colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
-  .push("violet")
+  array.push(string)
 end
 
 def using_unshift(array, string)
-  bouroughs_in_nyc = ["Brooklyn", "Queens", "Manhattan", "Bronx"]
-  .unshift("Staten Island")
+  array.unshift(string)
 end
 
 def using_pop(array)
-  @continents = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]
-  @continents.pop
+  array.pop
 end
 
 def pop_with_args(array)
-  @dog_breeds = ["Great Dane", "Golden Retriever", "Chihuahua", "Shiba Inu"]
-  @dog_breeds.pop(2)
+  array.pop(2)
 end
 
 def using_shift(array)
-  @my_favorite_cities = ["Lagos", "Cape Town", "Nairobi", "San Francisco", "Gaborone", "New York", "Berlin", "London"]
-  @my_favorite_cities.shift
+  array.shift
 end
 
 def shift_with_args(array)
-  @ice_cream_brands = ["Blue Bell Creameries", "Ben & Jerry's", "Baskin Robbins", "Braum's", "Breyer's"]
-  @ice_cream_brands.shift(2)
+  array.shift(2)
 end
 
 def using_concat(array1, array2)
-  @my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
-  more_favs = ["sports cars", "flatiron school"]
-  @my_favorite_things.concat(more_favs)
+  array1.concat(array2)
 end
 
 def using_insert(array, element)
-  @list_of_programming_languages = ["Ruby", "JavaScript", "Java", "C#", "Objective C", "C++", "PHP"]
-  @list_of_programming_languages.insert(4, "Python")
+  array.insert(4, element)
 end
 
 def using_uniq(array)
-  haircuts = ["Pixie", "Bob", "Mohawk", "Crew Cut", "Linka", "Wheeler", "Bob"]
-  haircuts.uniq
+  array.uniq
 end
 
 def using_flatten(array)
-  instruments = ["Saxophone", ["Piano", "Trumpet"], "Violin", "Drums", "Flute"]
-  instruments.flatten
+  array.flatten
 end
 
 def using_delete(array, string)
-  array.delete("Steven")
+  array.delete(string)
 end
 
 def using_delete_at(array, index)
-  famous_robots = ["Johnny 5", "R2D2", "Robocop"]
-  famous_robots.delete_at(2)
+  array.delete_at(2)
 end
