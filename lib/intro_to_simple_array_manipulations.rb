f64591ef902ff7b2938e29727f3958d831a608cf
Scott Hoisington <hoyzington@gmail.com>
2019-10-31 18:48:08 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -36,7 +36,7 @@ end
 
 def using_insert(array, element)
   @list_of_programming_languages = ["Ruby", "JavaScript", "Java", "C#", "Objective C", "C++", "PHP"]
-  .insert(3, "Python")
+  .insert(2, "Python")
 end
 
 def using_uniq()
