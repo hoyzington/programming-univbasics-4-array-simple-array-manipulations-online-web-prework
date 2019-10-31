b9424577cd4c0e7d6eae2fce08a769846ee95934
Scott Hoisington <hoyzington@gmail.com>
2019-10-31 18:52:56 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -40,7 +40,8 @@ def using_insert(array, element)
 end
 
 def using_uniq(array)
-  
+  haircuts = ["Pixie", "Bob", "Mohawk", "Crew Cut", "Linka", "Wheeler", "Bob"]
+  haircuts.uniq
 end
 
 def using_flatten(array)
