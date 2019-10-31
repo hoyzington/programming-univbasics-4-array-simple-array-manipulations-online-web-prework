16c42a1bb63a902fd56c82e7d5657248d9c797d7
Scott Hoisington <hoyzington@gmail.com>
2019-10-31 06:48:37 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,29 @@
+def using_push(array, string)
+  colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
+  .push("violet")
+end
+
+def using_unshift(array, string)
+  bouroughs_in_nyc = ["Brooklyn", "Queens", "Manhattan", "Bronx"]
+  .unshift("Staten Island")
+end
+
+def using_pop(array)
+  @continents = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]
+  .pop
+end
+
+def pop_with_args(array)
+  
+  
+end
+
+def using_shift(array)
+  
+  
+end
+
+def shift_with_args(array)
+  
+  
+end
